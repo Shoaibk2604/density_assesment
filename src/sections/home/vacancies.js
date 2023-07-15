4c63dec765f686614e826d14ed68aa75cb61e4dc
Shoaib <Shoaibk909@gmail.com>
2023-07-15 12:04:21 +0530

    Ui-done

--- src/sections/home/vacancies.js
+++ src/sections/home/vacancies.js
@@ -1,34 +1,78 @@
-import Iconify from '@/components/iconify'
-import { Box, Card, CardContent, Container, Grid, Typography } from '@mui/material'
-import React from 'react'
+import Iconify from "@/components/iconify";
+import {
+  Box,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React from "react";
 
 const Vacancies = () => {
   return (
     <React.Fragment>
-      <Box>
+      <Box py={6}>
         <Container>
-            <Box >
-                <Typography  variant='h2'>Open vacancies</Typography>
-            </Box>
-            <Grid container>
-                <Grid item lg={4}>
-                    <Card>
-                        <CardContent>
-                            <Box>
-                                <Typography variant='h5'>Senior Full-Stack Engineer</Typography>
-                            </Box>
-                            <Box>
-                            <Iconify icon="radix-icons:dot-filled"  />
-                                <Typography>Senior Full-Stack Engineer</Typography>
-                            </Box>
-                        </CardContent>
+          <Box py={6}>
+            <Typography variant="h2">Open vacancies</Typography>
+          </Box>
+          <Grid container spacing={4}>
+            {Array(3)
+              .fill()
+              .map((elem,index) => {
+                return (
+                  <Grid item lg={4} key={index}>
+                    <Card sx={{ backgroundColor: "#fff5fd", p: 1 }}>
+                      <CardContent>
+                        <Stack spacing={1.2}>
+                          <Box>
+                            <Typography variant="h5">
+                              Senior Full-Stack Engineer
+                            </Typography>
+                          </Box>
+                          <Stack direction="row" alignItems="center">
+                            <Iconify icon="radix-icons:dot-filled" />
+                            <Typography
+                              fontWeight={600}
+                              fontSize={14}
+                              color="grey"
+                            >
+                              Full-time position
+                            </Typography>
+                          </Stack>
+                          <Stack direction="row" alignItems="center">
+                            <Iconify icon="radix-icons:dot-filled" />
+                            <Typography
+                              fontWeight={600}
+                              fontSize={14}
+                              color="grey"
+                            >
+                              Berlin or remote
+                            </Typography>
+                          </Stack>
+                          <Stack direction="row" alignItems="center">
+                            <Iconify icon="radix-icons:dot-filled" />
+                            <Typography
+                              fontWeight={600}
+                              fontSize={14}
+                              color="grey"
+                            >
+                              $65-85k, 0.5-1.50% equity share options
+                            </Typography>
+                          </Stack>
+                        </Stack>
+                      </CardContent>
                     </Card>
-                </Grid>
-            </Grid>
+                  </Grid>
+                );
+              })}
+          </Grid>
         </Container>
       </Box>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Vacancies
+export default Vacancies;
